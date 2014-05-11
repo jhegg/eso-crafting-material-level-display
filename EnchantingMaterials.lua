1024036b124c117175a8dc55e5964dfa80f526d7
jhegg <josh.hegg@gmail.com>
2014-05-10 17:00:18 -0700

    Added Enchanting materials

---
+++ EnchantingMaterials.lua
@@ -0,0 +1,65 @@
+EnchantingMaterials = {
+    -- Additive Potency runes
+    [45855] = {tooltip = "Develop, Trifling, Potency Level 1, Gear Level 1 - 10"}, -- jora
+    [45856] = {tooltip = "Add, Inferior, Potency Level 1, Gear Level 5 - 15"}, -- porade
+    [45857] = {tooltip = "Increase, Petty, Potency Level 2, Gear Level 10 - 20"}, -- jera
+    [45806] = {tooltip = "Raise, Slight, Potency Level 2, Gear Level 15 - 25"}, -- jejora
+    [45807] = {tooltip = "Gain, Minor, Potency Level 3, Gear Level 20 - 30"}, -- odra
+    [45808] = {tooltip = "Supplement, Lesser, Potency Level 3, Gear Level 25 - 35"}, -- pojora
+    [45809] = {tooltip = "Boost, Moderate, Potency Level 4, Gear Level 30 - 40"}, -- edora
+    [45810] = {tooltip = "Advance, Average, Potency Level 4, Gear Level 35 - 45"}, -- jaera
+    [45811] = {tooltip = "Augment, Strong, Potency Level 5, Gear Level 40 - 50"}, -- pora
+    [45812] = {tooltip = "Strenghten, Great, Potency Level 5, Gear Level 45 - 50"}, -- denara
+    [45813] = {tooltip = "Exaggerate, Greater, Potency Level 6, Gear Level VR1 - VR3"}, -- rera
+    [45814] = {tooltip = "Empower, Greater, Potency Level 7, Gear Level VR3 - VR5"}, -- derado
+    [45815] = {tooltip = "Magnify, Grand, Potency Level 8, Gear Level VR5 - VR8"}, -- recura
+    [45816] = {tooltip = "Intensify, Monumental, Potency Level 9, Gear Level VR8 - VR10"}, -- cura
+
+    -- Subtractive Potency runes
+    [45817] = {tooltip = "Reduce, Trifling, Potency Level 1, Gear Level 1 - 10"}, -- jode
+    [45818] = {tooltip = "Subtract, Inferior, Potency Level 1, Gear Level 5 - 15"}, -- notade
+    [45819] = {tooltip = "Shrink, Petty, Potency Level 2, Gear Level 10 - 20"}, -- ode
+    [45820] = {tooltip = "Decrease, Slight, Potency Level 2, Gear Level 15 - 25"}, -- tade
+    [45821] = {tooltip = "Deduct, Minor, Potency Level 3, Gear Level 20 - 30"}, -- jayde
+    [45822] = {tooltip = "Lower, Lesser, Potency Level 3, Gear Level 25 - 35"}, -- edode
+    [45823] = {tooltip = "Diminish, Moderate, Potency Level 4, Gear Level 30 - 40"}, -- pojode
+    [45824] = {tooltip = "Weaken, Average, Potency Level 4, Gear Level 35 - 45"}, -- rekude
+    [45825] = {tooltip = "Lessen, Strong, Potency Level 5, Gear Level 40 - 50"}, -- hade
+    [45826] = {tooltip = "Impair, Great, Potency Level 5, Gear Level 45 - 50"}, -- idode
+    [45827] = {tooltip = "Remove, Greater, Potency Level 6, Gear Level VR1 - VR3"}, -- pode
+    [45828] = {tooltip = "Drain, Greater, Potency Level 7, Gear Level VR3 - VR5"}, -- kedeko
+    [45829] = {tooltip = "Deprive, Grand, Potency Level 8, Gear Level VR5 - VR8"}, -- rede
+    [45830] = {tooltip = "Negate, Monumental, Potency Level 9, Gear Level VR8 - VR10"}, -- kude
+
+    -- Essence runes for both Armor and Weapon Glyphs
+    [45833] = {tooltip = "For both Armor and Weapon Glyphs"}, -- deni
+    [45832] = {tooltip = "For both Armor and Weapon Glyphs"}, -- makko
+    [45831] = {tooltip = "For both Armor and Weapon Glyphs"}, -- oko
+
+    -- Essence runes for Weapon Glyphs
+    [45842] = {tooltip = "For Weapon Glyphs"}, -- deteri
+    [45843] = {tooltip = "For Weapon Glyphs"}, -- okori
+
+    -- Essence runes for both Weapon and Jewelry Glyphs
+    [45839] = {tooltip = "For Weapon Glyphs"}, -- dekeipa
+    [45841] = {tooltip = "For Weapon Glyphs"}, -- haoko
+    [45837] = {tooltip = "For Weapon Glyphs"}, -- kuoko
+    [45840] = {tooltip = "For Weapon Glyphs"}, -- meip
+    [45834] = {tooltip = "For Weapon Glyphs"}, -- okoma
+    [45838] = {tooltip = "For Weapon Glyphs"}, -- rakeipa
+
+    -- Essence runes for Jewelry Glyphs
+    [45836] = {tooltip = "For Jewelry Glyphs"}, -- denima
+    [45849] = {tooltip = "For Jewelry Glyphs"}, -- kaderi
+    [45848] = {tooltip = "For Jewelry Glyphs"}, -- makderi
+    [45835] = {tooltip = "For Jewelry Glyphs"}, -- makkoma
+    [45846] = {tooltip = "For Jewelry Glyphs"}, -- oru
+    [45847] = {tooltip = "For Jewelry Glyphs"}, -- taderi
+
+    -- Aspect runes
+    [45852] = {aspect = true}, -- denata
+    [45851] = {aspect = true}, -- jejota
+    [45854] = {aspect = true}, -- kuta
+    [45853] = {aspect = true}, -- rekuta
+    [45850] = {aspect = true}, -- ta
+}
