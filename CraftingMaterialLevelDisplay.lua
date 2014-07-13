442101a76f15a95c71daa4f3f525e7db6051e27b
jhegg <josh.hegg@gmail.com>
2014-07-13 13:14:07 -0700

    Enchanting improvements
    Updated enchanting tooltip text based on SilverWF's help. Also I'm now displaying the Potency rune levels directly in the inventory rows. If that gets in the way, it can be disabled in the addon settings menu.

--- CraftingMaterialLevelDisplay.lua
+++ CraftingMaterialLevelDisplay.lua
@@ -1,4 +1,4 @@
-local CraftingMaterialLevelDisplay = {
+CraftingMaterialLevelDisplay = {
     name = "CraftingMaterialLevelDisplay",
     savedVariables,
     defaultSavedVariables = {
@@ -6,7 +6,9 @@ local CraftingMaterialLevelDisplay = {
         provisioningFlavor = true,
         enchanting = true,
         alchemy = true,
+        enchantingInventoryList = true,
     },
+    slotLines = {}
 }
 
 local function AddTooltipLine(control, tooltipLine)
@@ -69,7 +71,7 @@ local function BuildAddonMenu()
 
     LAM:AddCheckbox(panelId,
         CraftingMaterialLevelDisplay.name.."ProvisioningCheckbox",
-        "Show Provisioning levels",
+        "Show Provisioning tooltips",
         nil,
         function() return CraftingMaterialLevelDisplay.savedVariables.provisioning end,
         function()
@@ -99,13 +101,25 @@ local function BuildAddonMenu()
 
     LAM:AddCheckbox(panelId,
         CraftingMaterialLevelDisplay.name.."EnchantingCheckbox",
-        "Show Enchanting levels",
+        "Show Enchanting tooltips",
         nil,
         function() return CraftingMaterialLevelDisplay.savedVariables.enchanting end,
         function()
             CraftingMaterialLevelDisplay.savedVariables.enchanting =
             not CraftingMaterialLevelDisplay.savedVariables.enchanting
         end)
+
+    LAM:AddCheckbox(panelId,
+        CraftingMaterialLevelDisplay.name.."EnchantingInvCheckbox",
+        "Show Enchanting levels in inventory",
+        nil,
+        function() return CraftingMaterialLevelDisplay.savedVariables.enchantingInventoryList end,
+        function()
+            CraftingMaterialLevelDisplay.savedVariables.enchantingInventoryList =
+            not CraftingMaterialLevelDisplay.savedVariables.enchantingInventoryList
+        end,
+        true,
+        "Requires /reloadui to take effect")
 end
 
 local function InitializeSavedVariables()
@@ -141,12 +155,82 @@ local function HookTooltips()
     end
 end
 
+local backpackListView = PLAYER_INVENTORY.inventories[1].listView
+local bankListView = PLAYER_INVENTORY.inventories[3].listView
+local guildBankListView = PLAYER_INVENTORY.inventories[4].listView
+local lootWindowListView = LOOT_WINDOW.list
+
+local inventoriesToItemLookupFunctions = {
+    [backpackListView] = {GetItemLink, "bagId", "slotIndex"},
+    [bankListView] = {GetItemLink, "bagId", "slotIndex"},
+    [guildBankListView] = {GetItemLink, "bagId", "slotIndex"},
+    [lootWindowListView] = {GetLootItemLink, "lootId", nil}
+}
+
+local function getLabelForInventoryRowControl(row)
+    local label = CraftingMaterialLevelDisplay.slotLines[row:GetName()]
+    if not label then
+        label = WINDOW_MANAGER:CreateControl(row:GetName() .. "CMLD", row, CT_LABEL)
+        CraftingMaterialLevelDisplay.slotLines[row:GetName()] = label
+    end
+    return label
+end
+
+local function GetItemIdFromLink(itemLink)
+    local itemId = select(4, ZO_LinkHandler_ParseLink(itemLink))
+    return tonumber(itemId)
+end
+
+local function AddEnchantingLevelToInventoryRow(rowControl, lookupFunctions)
+    local getItemLinkFunction = lookupFunctions[1]
+    local inventorySlot = rowControl.dataEntry.data
+    local bagId = inventorySlot[lookupFunctions[2]]
+    local inventorySlotIndex = lookupFunctions[3] and inventorySlot[lookupFunctions[3]] or nil
+    local itemId = GetItemIdFromLink(getItemLinkFunction(bagId, inventorySlotIndex))
+    local label = getLabelForInventoryRowControl(rowControl)
+    label:SetText(itemId)
+    label:SetFont("ZoFontGame")
+    label:ClearAnchors()
+    label:SetHidden(true)
+
+    local tradeSkillType, itemType = GetItemCraftingInfo(bagId, inventorySlotIndex)
+    if tradeSkillType == CRAFTING_TYPE_ENCHANTING
+            and itemType ~= ITEMTYPE_GLYPH_ARMOR
+            and itemType ~= ITEMTYPE_GLYPH_JEWELRY
+            and itemType ~= ITEMTYPE_GLYPH_WEAPON then
+        -- Does not need to account for the created Glyphs, just the runes
+        if CraftingMaterialLevelDisplay.savedVariables.enchanting then
+            if EnchantingMaterials[itemId].level ~= nil then
+                label:SetText(EnchantingMaterials[itemId].level)
+                label:SetAnchor(RIGHT, rowControl, RIGHT, -50)
+                label:SetHidden(false)
+            end
+        end
+    end
+
+end
+
+local function HookInventory()
+    for inventoryList, lookupFunctions in pairs(inventoriesToItemLookupFunctions) do
+        if inventoryList and inventoryList.dataTypes and inventoryList.dataTypes[1] then
+            local existingCallbackFunction = inventoryList.dataTypes[1].setupCallback
+            inventoryList.dataTypes[1].setupCallback = function(rowControl, slot)
+                existingCallbackFunction(rowControl, slot)
+                if CraftingMaterialLevelDisplay.savedVariables.enchantingInventoryList then
+                    AddEnchantingLevelToInventoryRow(rowControl, lookupFunctions)
+                end
+            end
+        end
+    end
+end
+
 local function onLoad(_, name)
     if name ~= CraftingMaterialLevelDisplay.name then return end
     EVENT_MANAGER:UnregisterForEvent(CraftingMaterialLevelDisplay.name, EVENT_ADD_ON_LOADED);
     InitializeSavedVariables()
     BuildAddonMenu()
     HookTooltips()
+    HookInventory()
 end
 
 EVENT_MANAGER:RegisterForEvent(CraftingMaterialLevelDisplay.name, EVENT_ADD_ON_LOADED, onLoad)
--- EnchantingMaterials.lua
+++ EnchantingMaterials.lua
@@ -1,62 +1,62 @@
 EnchantingMaterials = {
     -- Additive Potency runes
-    [45855] = {tooltip = "Level 1 - 10, Potency"}, -- jora
-    [45856] = {tooltip = "Level 5 - 15, Potency"}, -- porade
-    [45857] = {tooltip = "Level 10 - 20, Potency"}, -- jera
-    [45806] = {tooltip = "Level 15 - 20, Potency"}, -- jejora
-    [45807] = {tooltip = "Level 20 - 30, Potency"}, -- odra
-    [45808] = {tooltip = "Level 25 - 35, Potency"}, -- pojora
-    [45809] = {tooltip = "Level 30 - 40, Potency"}, -- edora
-    [45810] = {tooltip = "Level 35 - 45, Potency"}, -- jaera
-    [45811] = {tooltip = "Level 40 - 50, Potency"}, -- pora
-    [45812] = {tooltip = "Level VR1 - VR3, Potency"}, -- denara
-    [45813] = {tooltip = "Level VR3 - VR5, Potency"}, -- rera
-    [45814] = {tooltip = "Level VR5 - VR7, Potency"}, -- derado
-    [45815] = {tooltip = "Level VR7 - VR9, Potency"}, -- recura
-    [45816] = {tooltip = "Level VR10 - VR14, Potency"}, -- cura
+    [45855] = {tooltip = "Level 1 - 10, Additive", level = "1 - 10"}, -- jora
+    [45856] = {tooltip = "Level 5 - 15, Additive", level = "5 - 15"}, -- porade
+    [45857] = {tooltip = "Level 10 - 20, Additive", level = "10 - 20"}, -- jera
+    [45806] = {tooltip = "Level 15 - 20, Additive", level = "15 - 20"}, -- jejora
+    [45807] = {tooltip = "Level 20 - 30, Additive", level = "20 - 30"}, -- odra
+    [45808] = {tooltip = "Level 25 - 35, Additive", level = "25 - 35"}, -- pojora
+    [45809] = {tooltip = "Level 30 - 40, Additive", level = "30 - 40"}, -- edora
+    [45810] = {tooltip = "Level 35 - 45, Additive", level = "35 - 45"}, -- jaera
+    [45811] = {tooltip = "Level 40 - 50, Additive", level = "40 - 50"}, -- pora
+    [45812] = {tooltip = "Level VR1 - VR3, Additive", level = "VR1 - VR3"}, -- denara
+    [45813] = {tooltip = "Level VR3 - VR5, Additive", level = "VR3 - VR5"}, -- rera
+    [45814] = {tooltip = "Level VR5 - VR7, Additive", level = "VR5 - VR7"}, -- derado
+    [45815] = {tooltip = "Level VR7 - VR9, Additive", level = "VR7 - VR9"}, -- recura
+    [45816] = {tooltip = "Level VR10 - VR14, Additive", level = "VR10 - VR14"}, -- cura
 
     -- Subtractive Potency runes
-    [45817] = {tooltip = "Level 1 - 10, Negative potency"}, -- jode
-    [45818] = {tooltip = "Level 5 - 15, Negative potency"}, -- notade
-    [45819] = {tooltip = "Level 10 - 20, Negative potency"}, -- ode
-    [45820] = {tooltip = "Level 15 - 25, Negative potency"}, -- tade
-    [45821] = {tooltip = "Level 20 - 30, Negative potency"}, -- jayde
-    [45822] = {tooltip = "Level 25 - 35, Negative potency"}, -- edode
-    [45823] = {tooltip = "Level 30 - 40, Negative potency"}, -- pojode
-    [45824] = {tooltip = "Level 35 - 45, Negative potency"}, -- rekude
-    [45825] = {tooltip = "Level 40 - 50, Negative potency"}, -- hade
-    [45826] = {tooltip = "Level VR1 - VR3, Negative potency"}, -- idode
-    [45827] = {tooltip = "Level VR3 - VR5, Negative potency"}, -- pode
-    [45828] = {tooltip = "Level VR5 - VR7, Negative potency"}, -- kedeko
-    [45829] = {tooltip = "Level VR7 - VR9, Negative potency"}, -- rede
-    [45830] = {tooltip = "Level VR10 - VR14, Negative potency"}, -- kude
+    [45817] = {tooltip = "Level 1 - 10, Subtractive", level = "1 - 10"}, -- jode
+    [45818] = {tooltip = "Level 5 - 15, Subtractive", level = "5 - 15"}, -- notade
+    [45819] = {tooltip = "Level 10 - 20, Subtractive", level = "10 - 20"}, -- ode
+    [45820] = {tooltip = "Level 15 - 25, Subtractive", level = "15 - 25"}, -- tade
+    [45821] = {tooltip = "Level 20 - 30, Subtractive", level = "20 - 30"}, -- jayde
+    [45822] = {tooltip = "Level 25 - 35, Subtractive", level = "25 - 35"}, -- edode
+    [45823] = {tooltip = "Level 30 - 40, Subtractive", level = "30 - 40"}, -- pojode
+    [45824] = {tooltip = "Level 35 - 45, Subtractive", level = "35 - 45"}, -- rekude
+    [45825] = {tooltip = "Level 40 - 50, Subtractive", level = "40 - 50"}, -- hade
+    [45826] = {tooltip = "Level VR1 - VR3, Subtractive", level = "VR1 - VR3"}, -- idode
+    [45827] = {tooltip = "Level VR3 - VR5, Subtractive", level = "VR3 - VR5"}, -- pode
+    [45828] = {tooltip = "Level VR5 - VR7, Subtractive", level = "VR5 - VR7"}, -- kedeko
+    [45829] = {tooltip = "Level VR7 - VR9, Subtractive", level = "VR7 - VR9"}, -- rede
+    [45830] = {tooltip = "Level VR10 - VR14, Subtractive", level = "VR10 - VR14"}, -- kude
 
     -- Essence runes for both Armor and Weapon Glyphs
-    [45833] = {tooltip = "Stamina - Armor, Weapon"}, -- deni
-    [45832] = {tooltip = "Magicka - Armor, Weapon"}, -- makko
-    [45831] = {tooltip = "Health - Armor, Weapon"}, -- oko
+    [45833] = {tooltip = "Stamina - Armor, Weapon\n+ Adds max Stamina\n- Deals magic damage and restores Stamina"}, -- deni
+    [45832] = {tooltip = "Magicka - Armor, Weapon\n+ Adds max Magicka\n- Deals magic damage and restores Magicka"}, -- makko
+    [45831] = {tooltip = "Health - Armor, Weapon\n+ Adds max Health\n- Deals magic damage and restores Health"}, -- oko
 
     -- Essence runes for Weapon Glyphs
-    [45842] = {tooltip = "Armor - Weapon"}, -- deteri   
-    [45843] = {tooltip = "Power - Weapon"}, -- okori
+    [45842] = {tooltip = "Armor - Weapon\n+ Grants damage shield for 5 sec\n- Reduce target's armor for 5 sec"}, -- deteri
+    [45843] = {tooltip = "Power - Weapon\n+ Increase weapon damage for 5 sec\n- Reduce target weapon damage for 5 sec"}, -- okori
 
     -- Essence runes for both Weapon and Jewelry Glyphs
-    [45839] = {tooltip = "Frost - Weapon, Jewelry"}, -- dekeipa
-    [45841] = {tooltip = "Disease - Weapon, Jewelry"}, -- haoko
-    [45837] = {tooltip = "Poison - Weapon, Jewelry"}, -- kuoko
-    [45840] = {tooltip = "Shock - Weapon, Jewelry"}, -- meip
-    [45834] = {tooltip = "Health Regen - Weapon, Jewelry"}, -- okoma
-    [45838] = {tooltip = "Fire - Weapon, Jewelry"}, -- rakeipa
+    [45839] = {tooltip = "Frost - Weapon, Jewelry\n+ Deals frost damage\n- Adds frost resistance"}, -- dekeipa
+    [45841] = {tooltip = "Disease - Weapon, Jewelry\n+ Deals disease damage\n- Adds disease resistance"}, -- haoko
+    [45837] = {tooltip = "Poison - Weapon, Jewelry\n+ Deals poison damage\n- Adds poison resistance"}, -- kuoko
+    [45840] = {tooltip = "Shock - Weapon, Jewelry\n+ Deals shock damage\n- Adds shock resistance"}, -- meip
+    [45834] = {tooltip = "Health Regen - Jewelry, Weapon\n+ Adds health recovery\n- Deals unresistable damage"}, -- okoma
+    [45838] = {tooltip = "Fire - Weapon, Jewelry\n+ Deals flame damage\n- Adds flame resistance"}, -- rakeipa
 
     -- Essence runes for Jewelry Glyphs
-    [45836] = {tooltip = "Stamina Regen - Jewelry"}, -- denima
-    [45844] = {tooltip = "Bash - Jewelry"}, -- jaedi, removed or old rune
-    [45849] = {tooltip = "Bash - Jewelry"}, -- kaderi
-    [45845] = {tooltip = "Physical Harm - Jewelry"}, -- lire, removed or old rune
-    [45848] = {tooltip = "Spell Harm - Jewelry"}, -- makderi
-    [45835] = {tooltip = "Magicka Regen - Jewelry"}, -- makkoma
-    [45846] = {tooltip = "Alchemist - Jewelry"}, -- oru
-    [45847] = {tooltip = "Physical Harm - Jewelry"}, -- taderi
+    [45836] = {tooltip = "Stamina Regen - Jewelry\n+ Adds stamina recovery\n- Reduce stamina cost of abilities"}, -- denima
+    [45844] = {tooltip = "Bash - Jewelry\n+ Increase bash damage\n- Reduce cost of bash and blocking"}, -- jaedi, removed or old rune
+    [45849] = {tooltip = "Bash - Jewelry\n+ Increase bash damage\n- Reduce cost of bash and blocking"}, -- kaderi
+    [45845] = {tooltip = "Physical Harm - Jewelry\n+ Adds weapon damage\n- Adds armor"}, -- lire, removed or old rune
+    [45848] = {tooltip = "Spell Harm - Jewelry\n+ Adds spell damage\n- Adds spell resistance"}, -- makderi
+    [45835] = {tooltip = "Magicka Regen - Jewelry\n+ Adds magicka recovery\n- Reduce magicka cost of spells"}, -- makkoma
+    [45846] = {tooltip = "Alchemist - Jewelry\n+ Increase effect of restoration potions\n- Reduce cooldown of potions"}, -- oru
+    [45847] = {tooltip = "Physical Harm - Jewelry\n+ Adds weapon damage\n- Adds armor"}, -- taderi
 
     -- Aspect runes
     [45852] = {aspect = true}, -- denata
